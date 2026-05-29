c3e81bd5103175a255be62ba7bfe430a6bf84d0d
Roman Volovelskyi <vistauser1990@gmail.com>
2026-05-29 14:47:09 +0300

    build(mac-agent): add Dependencies, IssueReporting, TOMLKit, CustomDump

--- mac-agent/Package.swift
+++ mac-agent/Package.swift
@@ -8,10 +8,30 @@ let package = Package(
         .library(name: "LampAgent", targets: ["LampAgent"]),
         .executable(name: "lamp-agent", targets: ["lamp-agent"]),
     ],
-    dependencies: [],
+    dependencies: [
+        .package(url: "https://github.com/pointfreeco/swift-dependencies", from: "1.3.0"),
+        .package(url: "https://github.com/pointfreeco/xctest-dynamic-overlay", from: "1.4.0"),
+        .package(url: "https://github.com/LebJe/TOMLKit", from: "0.6.0"),
+        .package(url: "https://github.com/pointfreeco/swift-custom-dump", from: "1.3.0"),
+    ],
     targets: [
-        .target(name: "LampAgent"),
+        .target(
+            name: "LampAgent",
+            dependencies: [
+                .product(name: "Dependencies", package: "swift-dependencies"),
+                .product(name: "DependenciesMacros", package: "swift-dependencies"),
+                .product(name: "IssueReporting", package: "xctest-dynamic-overlay"),
+                .product(name: "TOMLKit", package: "TOMLKit"),
+            ]
+        ),
         .executableTarget(name: "lamp-agent", dependencies: ["LampAgent"]),
-        .testTarget(name: "LampAgentTests", dependencies: ["LampAgent"]),
+        .testTarget(
+            name: "LampAgentTests",
+            dependencies: [
+                "LampAgent",
+                .product(name: "Dependencies", package: "swift-dependencies"),
+                .product(name: "CustomDump", package: "swift-custom-dump"),
+            ]
+        ),
     ]
 )
